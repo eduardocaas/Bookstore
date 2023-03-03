4d70412cf595343a547d76528cc5c5aa889a9292
Eduardo Castro <eduardofc.dev@gmail.com>
2023-03-02 22:46:30 -0300

    Remoção de categoria implementado

---
+++ src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Categoria } from 'src/app/models/categoria';
+import { CategoriaService } from 'src/app/services/categoria.service';
+
+@Component({
+  selector: 'app-categoria-delete',
+  templateUrl: './categoria-delete.component.html',
+  styleUrls: ['./categoria-delete.component.css']
+})
+export class CategoriaDeleteComponent implements OnInit {
+
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(private service: CategoriaService, private route: ActivatedRoute, private router: Router, private toastr: ToastrService) {}
+
+  ngOnInit(): void {
+      this.categoria.id = this.route.snapshot.paramMap.get('id');
+      this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id).subscribe((response) => {
+      this.categoria.nome = response.nome;
+      this.categoria.descricao = response.descricao;
+    });
+  }
+
+  delete(): void {
+    this.service.delete(this.categoria.id).subscribe((response) => {
+      this.router.navigate(['categorias']);
+      this.toastr.success('Categoria ' + (this.categoria.nome).toUpperCase() + ' removida com sucesso!', 'Remoção', {timeOut: 6000});
+    }, err => {
+      this.toastr.error(err.error.error, 'Erro', {timeOut: 6000});
+    });
+  }
+
+}
