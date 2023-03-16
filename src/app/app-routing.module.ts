ebe67487997eacd821747d0ea67b3c57afe75dfc
Eduardo Castro <eduardofc.dev@gmail.com>
2023-03-15 23:34:14 -0300

    Componente livro-create gerado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
+import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
 
 const routes: Routes = [
   {path: "", component: NavComponent, children: [
@@ -15,7 +16,8 @@ const routes: Routes = [
   {path: "categorias/create" , component: CategoriaCreateComponent},
   {path: "categorias/delete/:id", component: CategoriaDeleteComponent},
   {path: "categorias/update/:id" , component: CategoriaUpdateComponent},
-  {path: "categorias/:id_cat/livros", component: LivroReadAllComponent}
+  {path: "categorias/:id_cat/livros", component: LivroReadAllComponent},
+  {path: "categorias/:id_cat/livros/create", component: LivroCreateComponent}
   ]
   }
 ];
---
+++ src/app/components/views/livro/livro-create/livro-create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-create',
+  templateUrl: './livro-create.component.html',
+  styleUrls: ['./livro-create.component.css']
+})
+export class LivroCreateComponent {
+
+
+//FormControl fazer maxlength de acordo com API
+
+}
