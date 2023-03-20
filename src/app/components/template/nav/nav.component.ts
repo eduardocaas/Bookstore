9fa578e92bccc8a2c912f71db181eaf5ee3a16d0
Eduardo Castro <eduardofc.dev@gmail.com>
2023-03-19 22:41:45 -0300

    Update nav.component.ts

--- src/app/components/template/nav/nav.component.ts
+++ src/app/components/template/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-      this.router.navigate(['livros/delete/4']);
+      this.router.navigate(['livros/delete/2/4']);
   }
 
 }
