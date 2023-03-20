bdb96f53200c9899dcd9a94cdb6c6b3318fa8359
Eduardo Castro <eduardofc.dev@gmail.com>
2023-03-19 22:23:58 -0300

    Rotas para livro-delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { CategoriaDeleteComponent } from './components/views/categoria/categoria
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
+import { LivroDeleteComponent } from './components/views/livro/livro-delete/livro-delete.component';
 
 const routes: Routes = [
   {path: "", component: NavComponent, children: [
@@ -17,7 +18,8 @@ const routes: Routes = [
   {path: "categorias/delete/:id", component: CategoriaDeleteComponent},
   {path: "categorias/update/:id" , component: CategoriaUpdateComponent},
   {path: "categorias/:id_cat/livros", component: LivroReadAllComponent},
-  {path: "categorias/:id_cat/livros/create", component: LivroCreateComponent}
+  {path: "categorias/:id_cat/livros/create", component: LivroCreateComponent},
+  {path: "livros/delete/:id", component: LivroDeleteComponent}
   ]
   }
 ];
