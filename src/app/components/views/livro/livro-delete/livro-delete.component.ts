ae89eb418a540d615c9381f17fff3c4d74719bb5
Eduardo Castro <eduardofc.dev@gmail.com>
2023-03-19 22:43:02 -0300

    Tratamento de parâmetro recebido na URL

--- src/app/components/views/livro/livro-delete/livro-delete.component.ts
+++ src/app/components/views/livro/livro-delete/livro-delete.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Livro } from 'src/app/models/livro';
 
 @Component({
@@ -6,12 +7,20 @@ import { Livro } from 'src/app/models/livro';
   templateUrl: './livro-delete.component.html',
   styleUrls: ['./livro-delete.component.css']
 })
-export class LivroDeleteComponent {
+export class LivroDeleteComponent implements OnInit {
+  
+  id_cat: any = '';
 
   livro: Livro = {
     id: '',
     titulo: '',
     nome_autor: '',
     texto: ''
+  }
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat');
   }
 }
