6e8eae9e009af78634635d86494fe4cdced9f725
Eduardo Castro <eduardofc.dev@gmail.com>
2023-03-21 23:04:25 -0300

    Métodos de validação e instanciação de objeto

--- src/app/components/views/livro/livro-update/livro-update.component.ts
+++ src/app/components/views/livro/livro-update/livro-update.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Livro } from 'src/app/models/livro';
+import { LivroService } from 'src/app/services/livro.service';
 
 @Component({
   selector: 'app-livro-update',
   templateUrl: './livro-update.component.html',
   styleUrls: ['./livro-update.component.css']
 })
-export class LivroUpdateComponent {
+export class LivroUpdateComponent implements OnInit{
+
+  id_cat: any = '';
+
+  livro: Livro = {
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
+  }
+
+  constructor(private route: ActivatedRoute, private service: LivroService) {}
+
+  titulo: FormControl = new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(50)]);
+  autor: FormControl = new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(50)]);
+  texto: FormControl = new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(1000)]);
+
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat');
+    this.livro.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.livro.id).subscribe(response => {
+      this.livro.titulo = response.titulo;
+      this.livro.nome_autor = response.nome_autor;
+      this.livro.texto = response.texto;
+    });
+  }
+
+  validFields(): boolean {
+    return (this.titulo.valid && this.titulo.value.length >= 2 && this.titulo.value.length <= 50) && 
+           (this.autor.valid && this.autor.value.length >= 2 && this.autor.value.length <= 50) &&
+           (this.texto.valid && this.texto.value.length >= 2 && this.texto.value.length <= 1000);
+  }
 
 }
